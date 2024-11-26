844140abb9027c3e4d748f5f78e23f0901256c86
Ramotala Karabo <KaraboJaz03@gmail.com>
2024-11-26 05:29:40 +0200

    update the exisiting codebase with a working feature

---
+++ blogposts/src/App.jsx
@@ -0,0 +1,15 @@
+import BlogPosts from '../Components/BlogPosts'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <div className="App">
+            <BlogPosts />
+        </div>
+    </>
+  )
+}
+
+export default App
